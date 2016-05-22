1082810e947a8a8b3113fe4745e45041da47d51f
Kyriacos Kyriacou <kyriacos.s.kyriacou@gmail.com>
2016-05-22 20:25:46 +0300

    Add routerdata class (to be used as data for a node)

---
+++ Flooding/RouterData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flooding
+{
+    class RouterData : IEquatable<RouterData>
+    {
+        private char _id;
+        private readonly int _maxCapacity = 100;
+
+        public RouterData(char id)
+        {
+            _id = id;
+        }
+
+        #region Properties
+
+        public char ID => _id;
+        /// <summary>
+        /// Maximum packets the router can hold
+        /// </summary>
+        public int MaxCapacity => _maxCapacity;
+
+        #endregion
+
+        public bool Equals(RouterData other) => _id == other.ID;
+
+        public override string ToString() => _id.ToString();
+    }
+}
